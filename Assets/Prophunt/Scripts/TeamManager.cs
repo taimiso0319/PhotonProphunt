9eefd8afea447b8f513c2f94b87f3e6e782511f6
Keisuke Yasuda <keisuke0319@gmail.com>
2015-05-04 03:31:30 +0900

    Add TeamSystem

--- Assets/Prophunt/Scripts/TeamManager.cs
+++ Assets/Prophunt/Scripts/TeamManager.cs
@@ -3,22 +3,49 @@ using System.Collections;
 
 public class TeamManager : Photon.MonoBehaviour {
 	// Use this for initialization
-	public int team1Count;
-	public int team2Count;
+	public int redCount;
+	public int blueCount;
+	public int playerCount;
+	public bool editTest = false;
+	Room room;
 	void Start () {
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 
 	public void AddPlayer(GameObject playerObj){
-		int teamNum = PhotonNetwork.countOfPlayersInRooms%2;
-		if(team1Count == team2Count&&teamNum == 0)team1Count++;
-		else if(team1Count != team2Count && team1Count > team2Count && teamNum == 0){teamNum = 1; team2Count++;}
-		else if(team1Count != team2Count && team1Count < team2Count && teamNum == 1){teamNum = 0; team1Count++;}
+		int teamNum = 0;
+		room = PhotonNetwork.room;
+		playerCount = room.playerCount;
+		if(playerCount%2 == 0)teamNum = 0;
+		else if(playerCount%2 == 1)teamNum = 1;
 		playerObj.GetComponent<Prophunt.SDUnitychan.Status.PlayerStatusManager>().teamNum = teamNum;
+	}
+
+	public void RemovePlayer(GameObject playerObj){
+
+	}
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if(!editTest){
+			if (stream.isWriting)
+			{
+				// We own this player: send the others our data
+				//nameText.text = playerName;
+				stream.SendNext(playerCount);
+				stream.SendNext(redCount);
+				stream.SendNext(blueCount);
+			}
+			else
+			{
+				// Network player, receive data
+				this.playerCount = (int)stream.ReceiveNext();
+				this.redCount = (int)stream.ReceiveNext();
+				this.blueCount = (int)stream.ReceiveNext();
+				//nameText.text = playerName;
+			}
+		}
 	}
 }
