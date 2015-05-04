7de6586bce29ff0f6609910fd8fcc990d43ac7ad
Keisuke Yasuda <keisuke0319@gmail.com>
2015-05-04 18:42:52 +0900

    fix raycast

--- Assets/Prophunt/Scripts/UniRayCaster.cs
+++ Assets/Prophunt/Scripts/UniRayCaster.cs
@@ -4,14 +4,18 @@ using System.Collections;
 
 public class UniRayCaster : Photon.MonoBehaviour {
 	float timer = 0;
+	float limitTimer = 0;
 	bool countStart = false;
 	void Update(){
-		if(Physics.Raycast(transform.position, Vector3.down, transform.localScale.y/2 + 0.01f)){
-			countStart = true;
+		if(!countStart){
+			if(Physics.Raycast(transform.position, Vector3.down, transform.localScale.y/2 + 0.01f)){
+				countStart = true;
+			}
 		}
 	}
 	void FixedUpdate(){
 		if(countStart)timer += Time.deltaTime;
-		if(timer > 3) PhotonNetwork.Destroy(this.gameObject);
+		else limitTimer += Time.deltaTime;
+		if(timer > 3||limitTimer > 30) PhotonNetwork.Destroy(this.gameObject);
 	}
 }
